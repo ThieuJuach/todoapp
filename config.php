6c68f674290a245c70c522e9721a24a15142109b
thieujuach <juachandrewthieu@gmail.com>
2024-04-09 15:37:57 +0300

    renamed the folder

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     ini_set('display_errors', 1);
     
      // Database connection parameters
-     $host = "localhost";
+    /* $host = "localhost";
      $username = "root"; 
      $password = ""; 
      $database = "todo"; 
@@ -14,5 +14,15 @@
      // Check if the connection was successful
      if (!$conn) {
          die("Connection failed: " . mysqli_connect_error());
-     }
+     }*/
+
+     // PHP Data Objects(PDO) Sample Code:
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:atj.database.windows.net,1433; Database = todo", "ajuach", "Andy.664298");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+    }
 ?>
